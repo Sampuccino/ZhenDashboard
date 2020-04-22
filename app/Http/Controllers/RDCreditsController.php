c6f8ecd3ca721189110e09b92faebcfad9457ed6
Sampuccino <Sampuccino@gmail.com>
2020-04-22 01:09:04 -0700

    Implement Models and DB Schemas. Closes #13.

---
+++ app/Http/Controllers/RDCreditsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RDCredit;
+use Illuminate\Http\Request;
+
+class RDCreditsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RDCredit  $rDCredits
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RDCredit $rDCredits)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RDCredit  $rDCredits
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RDCredit $rDCredits)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RDCredit  $rDCredits
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RDCredit $rDCredits)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RDCredit  $rDCredits
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RDCredit $rDCredits)
+    {
+        //
+    }
+}
