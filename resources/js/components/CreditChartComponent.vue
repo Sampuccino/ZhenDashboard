59392d5ce73db5bbced06fdc4e8489154f9e8e2b
Sampuccino <Sampuccino@gmail.com>
2020-04-23 12:04:15 -0700

    Implement dynamic chart. Closes #19

--- resources/js/components/CreditChartComponent.vue
+++ resources/js/components/CreditChartComponent.vue
@@ -1,24 +1,51 @@
 <template>
   <div>
-    <canvas id="myChart" width="400" height="400"></canvas>
+    <div class="row">
+      <div class="col-4 text-center my-auto">
+        <h6 class="font-weight-bold">Total Credits</h6>
+        <h4>{{ returnTotalCompanyCreditsAvailable }}</h4>
+      </div>
+      <div class="col-4 text-center my-auto">
+        <h6 class="font-weight-bold">Credits Filed</h6>
+        <h4>{{ returnTotalCompanyCreditsClaimed }}</h4>
+      </div>
+      <div class="col-4 text-center my-auto">
+        <h6 class="font-weight-bold">Credits Received</h6>
+        <h4>{{ returnTotalCompanyCreditsReceived }}</h4>
+      </div>
+    </div>
+
+    <canvas ref="canvas" id="myChart" width="400" height="400"></canvas>
+
   </div>
 </template>
 
 <script>
-  import Chart from "chart.js";
+  import {Bar} from 'vue-chartjs'
+  import {mapGetters} from "vuex";
 
   export default {
+    extends: Bar,
     name: "CreditsChartComponent",
     mounted() {
-      let ctx = document.getElementById('myChart').getContext('2d');
-      let myChart = new Chart(ctx, {
-        // type: 'horizontalBar',
-        type: 'bar',
-        data: {
+      this.renderBarChart()
+    },
+    beforeUpdate(){
+      this.$data._chart.destroy();
+      this.renderBarChart()
+    },
+    computed: {
+      ...mapGetters(['returnTotalCompanyCreditsAvailable',
+        'returnTotalCompanyCreditsClaimed',
+        'returnTotalCompanyCreditsReceived']),
+    },
+    methods: {
+      renderBarChart() {
+        this.renderChart({
           labels: ['Total Credits', 'Credits Filed', 'Credits Recieved'],
-          datasets: [{
+            datasets: [{
             label: '# of Votes',
-            data: [19, 12, 3],
+            data: [this.returnTotalCompanyCreditsAvailable, this.returnTotalCompanyCreditsClaimed, this.returnTotalCompanyCreditsReceived],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -31,17 +58,11 @@
             ],
             borderWidth: 1
           }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
+        },{
+          responsive: false,
+          maintainAspectRatio: true
+        })
+      }
     }
   }
 </script>
--- resources/js/store.js
+++ resources/js/store.js
@@ -6,7 +6,14 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     intro: 'Welcome to Application Container',
-    selectedMenu: 2
+    selectedMenu: 2,
+    companies: [],
+    company: {},
+    companyRaD: [],
+    companyForm: [],
+    creditsAvailable: 0,
+    creditsClaimed: 0,
+    creditsReceived: 0,
   },
   getters: {
     returnIntro: state => {
@@ -14,20 +21,58 @@ export default new Vuex.Store({
     },
     returnSelectedMenu: state => {
       return state.selectedMenu;
+    },
+    returnTotalCompanyCreditsAvailable: state => {
+      // credit_available:
+      const creditsAvailableArr = [];
+      const creditsArr = state.companyRaD.forEach( c => { creditsAvailableArr.push(parseFloat(c.credit_available)) });
+      return creditsAvailableArr.reduce((a,b) => a+b,0) || 5;
+    },
+    returnTotalCompanyCreditsClaimed: state => {
+      // credit_claimed:
+      const creditsClaimedArr = [];
+      const creditsArr = state.companyRaD.forEach( c => { creditsClaimedArr.push(parseFloat(c.credit_claimed)) });
+      return creditsClaimedArr.reduce((a,b) => a+b,0) || 3;
+    },
+    returnTotalCompanyCreditsReceived: state => {
+      // credit_received:
+      const creditsReceivedArr = [];
+      const creditsArr = state.companyRaD.forEach( c => { creditsReceivedArr.push(parseFloat(c.credit_received)) });
+      return creditsReceivedArr.reduce((a,b) => a+b,0) || 1;
+    },
+    returnThreeChartDataPieces: () => {
+      return [this.returnTotalCompanyCreditsAvailable, this.returnTotalCompanyCreditsClaimed, this.returnTotalCompanyCreditsReceived]
     }
   },
   mutations: {
     setSelectedMenu:  (state, payload) => {
       state.selectedMenu = payload.selected;
+    },
+    setCompaniesList: (state, payload) => {
+      state.companies = payload;
+    },
+    handleSelectedCompany: (state, payload) => {
+      /* Filtering done based on ein payload */
+      state.company = state.companies.filter( c => {
+        return c.ein === payload;
+      });
+
+      // Set company R&D
+      state.companyRaD = state.company[0].research_and_development;
+
+      console.warn('Mutation handleSelectedCompany ', state.company)
+
     }
   },
   actions: {
     setSelectedMenu: (context, payload) => {
       context.commit('setSelectedMenu', {selected: parseInt(payload)})
+    },
+    setCompaniesList: (context, payload) => {
+      context.commit('setCompaniesList', payload);
+    },
+    handleSelectedCompany: (context, payload) => {
+      context.commit('handleSelectedCompany', payload);
     }
-    // async queueCsvTemplateFiles(context) {
-    //   let result = await axios.get('/files');
-    //   context.commit("queueCsvTemplateFiles", result.data);
-    // }
   }
 })
